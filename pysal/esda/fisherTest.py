fc9b247b490c2e18541ac65d2ed57bb5b55fd765
kx.knight@gmail.com <kx.knight@gmail.com@363a8f54-61ba-11de-bb41-d1906ad598ae>
2012-02-20 20:19:05 +0000

    multiprocessing performance problem solved by NOT USING NUMPY in the parallel code

--- pysal/esda/fisherTest.py
+++ pysal/esda/fisherTest.py
@@ -1,12 +1,21 @@
-from mapclassify import Fisher_Jenks, PFisher_Jenks, PFisher_Jenks_MP, PFisher_Jenks_PP
+from mapclassify import Fisher_Jenks_1, PFisher_Jenks, PFisher_Jenks_MP, PFisher_Jenks_PP
 import numpy as np
 
-dat = np.random.rand(2000)
+if __name__ == "__main__":
+    dat = np.random.rand(2000)
 
-o1 = Fisher_Jenks(dat)
+    print "Sequential\n"
+    o1 = Fisher_Jenks_1(dat)
+    print o1
 
-o2 = PFisher_Jenks(dat)
+    print "PyOpenCL\n"
+    o2 = PFisher_Jenks(dat)
+    print o2
 
-o3 = PFisher_Jenks_MP(dat)
-
-o4 = PFisher_Jenks_PP(dat)
+    print "Multiprocessing\n"
+    o3 = PFisher_Jenks_MP(dat)
+    print o3
+    
+    print "Parallel Python\n"
+    o4 = PFisher_Jenks_PP(dat)
+    print o4
