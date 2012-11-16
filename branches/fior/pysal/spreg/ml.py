60b07f5b25329f991202e74970dba0b8fc36b245
sjsrey@gmail.com <sjsrey@gmail.com@363a8f54-61ba-11de-bb41-d1906ad598ae>
2012-11-16 22:05:51 +0000

    Branching from trunk to fior (fileio refactoring) at 1334

---
+++ branches/fior/pysal/spreg/ml.py
@@ -0,0 +1,261 @@
+"""
+Maximum likelihood estimation of spatial process models
+
+
+TODO
+----
+
+ - check log likelihood for lag model and columnbus vesus opengeoda
+ - check ORD versus FULL likelihood results with sparse implementation for ORD
+ - asymptotic standard errors for ML_Lag
+ - ML_error likelihood
+ - ML_error partial
+ - ML_error estimation
+
+"""
+
+__author__ = "Serge Rey srey@asu.edu, Luc Anselin luc.anselin@asu.edu"
+
+import numpy as np
+import pysal as ps
+import scipy.sparse as SPARSE
+
+
+def defl_lag(r, w, e1, e2):
+    """
+    Derivative of the likelihood for the lag model
+
+    Parameters
+    ----------
+
+    r: estimate of rho
+
+    w: spatial weights object
+
+    e1: ols residuals of y on X 
+
+    e2: ols residuals of Wy on X
+
+    Returns
+    -------
+
+    tuple: 
+        tuple[0]: dfl (scalar) value of derivative at parameter values
+        tuple[1]: tr (scalar) trace of  (I-rW)^{-1} W
+
+    """
+    n = w.n
+    e1re2 = e1 - r * e2
+    num = np.dot(e1re2.T, e2)
+    den = np.dot(e1re2.T, e1re2)
+    a = -r * w.full()[0]
+    np.fill_diagonal(a, np.ones((w.n,1)))
+    a = np.linalg.inv(a)
+    tr = np.trace(np.dot(a, w.full()[0]))
+    dfl = n * (num/den) - tr
+    return dfl[0][0], tr
+
+def like_lag_full(r, e1, e2, w):
+    """
+    Log concentrated likelihood for the lag model using full evaluation
+    """
+
+    n = w.n
+    e1re2 = e1 - r*e2
+    ldet = _logJacobian(w, r)
+    return -(n/2.)  * np.log( (e1re2**2).sum() / n  ) + ldet
+
+
+def like_lag_ord(r, e1, e2, evals):
+    """
+    Log concentrated likelihood for the lag model using Ord (1975)
+    approximation
+
+    Parameters
+    ----------
+
+    r: estimate of rho
+
+    e1: ols residuals of y on X 
+
+    e2: ols residuals of Wy on X
+
+    evals: vector of eigenvalues of W
+
+
+    Returns
+    -------
+
+    lc: scalar value of concentrated likelihood
+
+    """
+    n = w.n
+    e1re2 = e1 - r*e2
+    revals = r * evals
+    return -(n/2.)  * np.log( (e1re2**2).sum() / n  ) + np.log(1-revals).sum()
+
+
+def _logJacobian(w, rho):
+    """
+    Log determinant of I-\rho W
+
+    Brute force initially
+    """
+
+    return np.log(np.linalg.det(np.eye(w.n) - rho * w.full()[0]))
+
+def symmetrize(w):
+    """Generate symmetric matrix that has same eigenvalues as unsymmetric row
+    standardized matrix w
+
+    Parameters
+    ----------
+    w: weights object that has been row standardized
+
+    Returns
+    -------
+    a sparse symmetric matrix with same eigenvalues as w
+    
+    """
+    current = w.transform
+    w.transform = 'B'
+    d = w.sparse.sum(axis=1) # row sum
+    d.shape=(w.n,)
+    D12 = SPARSE.spdiags(np.sqrt(1./d),[0],w.n,w.n)
+    w.transform = current
+    return D12*w.sparse*D12
+
+
+
+def ML_Lag(y, w, X, precrit=0.0000001, verbose=False, like='full'):
+    """
+    Maximum likelihood estimation of spatial lag model
+
+    y: dependent variabl (nx1 array)
+
+    w: spatial weights object
+
+    X: explanatory variables (nxk array)
+
+    precrit: convergence criterion
+
+    verbose: boolen to print iterations in estimation
+
+    like: method to use for evaluating concentrated likelihood function
+    (FULL|ORD) where FULL=Brute Force, ORD = eigenvalue based jacobian
+    """
+
+    # step 1 OLS of X on y yields b1
+    d = np.linalg.inv(np.dot(X.T, X))
+    b1 = np.dot(d, np.dot(X.T, y))
+
+    # step 2 OLS of X on Wy: yields b2
+    wy = ps.lag_spatial(w,y)
+    b2 = np.dot(d, np.dot(X.T, wy))
+
+    # step 3 compute residuals e1, e2
+    e1 = y - np.dot(X,b1)
+    e2 = wy - np.dot(X,b2)
+
+    # step 4 given e1, e2 find rho that maximizes Lc
+
+    # ols estimate of rho
+    XA = np.hstack((wy,X))
+    bols = np.dot(np.linalg.inv(np.dot(XA.T, XA)), np.dot(XA.T,y))
+    rols = bols[0][0]
+
+    while np.abs(rols) > 1.0:
+        rols = rols/2.0
+
+    if rols > 0.0:
+        r1 = rols
+        r2 = r1 / 5.0
+    else:
+        r2 = rols
+        r1 = r2 / 5.0
+
+    df1 = 0
+    df2 = 0
+    tr = 0
+    df1, tr = defl_lag(r1, w, e1, e2)
+    df2, tr = defl_lag(r2, w, e1, e2)
+
+    if df1*df2 <= 0:
+        ll = r2
+        ul = r1
+    elif df1 >= 0.0 and df1 >= df2:
+        ll = -0.999
+        ul = r2
+        df1 = df2
+        df2 = -(10.0**10)
+    elif df1 >= 0.0 and df1 < df2:
+        ll = r1
+        ul = 0.999
+        df2 = df1
+        df1 = -(10.0**10)
+    elif df1 < 0.0 and df1 >= df2:
+        ul = 0.999
+        ll = r1
+        df2 = df1
+        df1 = 10.0**10
+    else:
+        ul = r2
+        ll = -0.999
+        df1 = df2
+        df2 = 10.0**10
+
+    # main bisection iteration
+
+    err = 10
+    t = rols
+    ro = (ll+ul) / 2.
+    if verbose:
+        line ="%-5s\t%12s\t%12s\t%12s\t%12s"%("Iter.","LL","RHO","UL","DFR")
+        print line
+
+    i = 0
+    while err > precrit:
+        if verbose:
+            print "%d\t%12.8f\t%12.8f\t%12.8f\t%12.8f"  % (i,ll, ro, ul, df1)
+        dfr, tr = defl_lag(ro, w, e1, e2)
+        if dfr*df1 < 0.0:
+            ll = ro
+        else:
+            ul = ro
+            df1 = dfr
+        err = np.abs(t-ro)
+        t = ro
+        ro =(ul+ll)/2.
+        i += 1
+    ro = t
+    tr1 = tr
+    bml = b1 - (ro * b2)
+    b = [ro,bml]
+
+    # Likelihood evaluation
+
+    if like.upper() == 'ORD':
+        evals = SPARSE.linalg.eigsh(symmetrize(w))[0]
+        llik = like_lag_ord(ro, e1, e2, evals)
+    elif like.upper() == 'FULL':
+        llik = like_lag_full(ro, e1, e2, w)
+
+    return (b, tr1, llik)
+
+
+
+if __name__ == '__main__':
+
+    db =  ps.open(ps.examples.get_path('columbus.dbf'),'r')
+    y = np.array(db.by_col("CRIME"))
+    y.shape = (len(y),1)
+    X = []
+    X.append(db.by_col("INC"))
+    X.append(db.by_col("HOVAL"))
+    X = np.array(X).T
+    #w = ps.open(ps.examples.get_path("columbus.gal")).read()
+    w = ps.rook_from_shapefile(ps.examples.get_path("columbus.shp"))
+    w.transform = 'r'
+    X = np.hstack((np.ones((w.n,1)),X))
+    bml = ML_Lag(y,w,X, verbose=True, like='ORD')
+    bmlf = ML_Lag(y,w,X, verbose=True, like='FULL')
---
+++ branches/fior/pysal/weights/_contW_binning.py
@@ -0,0 +1,164 @@
+#!/usr/bin/python
+#import math
+import pysal
+from pysal.cg.standalone import get_shared_segments
+
+__author__ = "Sergio J. Rey <srey@asu.edu> "
+__all__ = ["QUEEN", "ROOK", "ContiguityWeights_binning"]
+
+
+# delta to get buckets right
+DELTA = 0.000001
+
+QUEEN = 1
+ROOK = 2
+
+# constants for bucket sizes
+BUCK_SM = 8
+BUCK_LG = 80
+SHP_SMALL = 1000
+
+
+def bbcommon(bb, bbother):
+    """
+    Checks for overlaps of bounding boxes. First, east-west, then north-south.
+    Element 0 is west, element 2 is east, element 1 is north?, element 3 is
+    south?
+    All four checks must be false for chflag to be true, meaning the two
+    bounding boxes do not overlap.
+    """
+    chflag = 0
+    if not ((bbother[2] < bb[0]) or (bbother[0] > bb[2])):
+        if not ((bbother[3] < bb[1]) or (bbother[1] > bb[3])):
+            chflag = 1
+    return chflag
+
+
+class ContiguityWeights_binning:
+    """ """
+    def __init__(self, shpFileObject, wttype):
+        self.shpFileObject = shpFileObject
+        self.wttype = wttype
+        self.bucket()
+        self.doWeights()
+
+    def bucket(self):
+        shpFileObject = self.shpFileObject
+
+        if shpFileObject.type != pysal.cg.Polygon:
+            return False
+
+        shapebox = shpFileObject.bbox      # bounding box
+
+        numPoly = len(shpFileObject)
+        self.numPoly = numPoly
+
+        # bucket size
+        if (numPoly < SHP_SMALL):
+            bucketmin = numPoly / BUCK_SM + 2
+        else:
+            bucketmin = numPoly / BUCK_LG + 2
+        # bucket length
+        lengthx = ((shapebox[2] + DELTA) - shapebox[0]) / bucketmin
+        lengthy = ((shapebox[3] + DELTA) - shapebox[1]) / bucketmin
+
+        # initialize buckets
+        columns = [set() for i in range(bucketmin)]
+        rows = [set() for i in range(bucketmin)]
+
+        minbox = shapebox[:2] * \
+            2                                  # minx,miny,minx,miny
+        binWidth = [lengthx, lengthy] * \
+            2                              # lenx,leny,lenx,leny
+        bbcache = {}
+        poly2Column = [set() for i in range(numPoly)]
+        poly2Row = [set() for i in range(numPoly)]
+        for i in range(numPoly):
+            shpObj = shpFileObject.get(i)
+            bbcache[i] = shpObj.bounding_box[:]
+            projBBox = [int((shpObj.bounding_box[:][j] -
+                             minbox[j]) / binWidth[j]) for j in xrange(4)]
+            for j in range(projBBox[0], projBBox[2] + 1):
+                columns[j].add(i)
+                poly2Column[i].add(j)
+            for j in range(projBBox[1], projBBox[3] + 1):
+                rows[j].add(i)
+                poly2Row[i].add(j)
+        # loop over polygons rather than bins
+        w = {}
+        for polyId in xrange(numPoly):
+            idRows = poly2Row[polyId]
+            idCols = poly2Column[polyId]
+            rowPotentialNeighbors = set()
+            colPotentialNeighbors = set()
+            for row in idRows:
+                rowPotentialNeighbors = rowPotentialNeighbors.union(rows[row])
+            for col in idCols:
+                colPotentialNeighbors = colPotentialNeighbors.union(
+                    columns[col])
+            potentialNeighbors = rowPotentialNeighbors.intersection(
+                colPotentialNeighbors)
+            if polyId not in w:
+                w[polyId] = set()
+            for j in potentialNeighbors:
+                if polyId < j:
+                    if bbcommon(bbcache[polyId], bbcache[j]):
+                        w[polyId].add(j)
+
+        self.potentialW = w
+
+    def doWeights(self):
+        pw = self.potentialW
+        polygonCache = {}
+        w = {}
+        shpFileObject = self.shpFileObject
+        for polyId in xrange(self.numPoly):
+            if polyId not in polygonCache:
+                iVerts = set(shpFileObject.get(polyId).vertices)
+                polygonCache[polyId] = iVerts
+            else:
+                iVerts = polygonCache[polyId]
+            potentialNeighbors = pw[polyId]
+            if polyId not in w:
+                w[polyId] = set()
+            for j in potentialNeighbors:
+                if j not in polygonCache:
+                    polygonCache[j] = set(shpFileObject.get(j).vertices)
+                common = iVerts.intersection(polygonCache[j])
+                join = False
+                if len(common) > 1:  # ROOK
+                    #double check rook
+                    poly0 = shpFileObject.get(polyId)
+                    poly1 = shpFileObject.get(j)
+                    if get_shared_segments(poly0, poly1, True):
+                        join = True
+                    #for vert in common:
+                    #    idx = poly0.vertices.index(vert)
+                    #    IDX = poly1.vertices.index(vert)
+                    #    try:
+                    #        if poly0.vertices[idx+1] == poly1.vertices[IDX+1] or poly0.vertices[idx+1] == poly1.vertices[IDX-1]\
+                    #        or poly0.vertices[idx-1] == poly1.vertices[IDX+1] or poly0.vertices[idx-1] == poly1.vertices[IDX-1]:
+                    #            join = True
+                    #            break
+                    #    except IndexError:
+                    #        pass
+                if len(common) > 0:  # QUEEN
+                    if self.wttype == QUEEN:
+                        join = True
+                if join:
+                    w[polyId].add(j)
+                    if j not in w:
+                        w[j] = set()
+                    w[j].add(polyId)
+
+            del polygonCache[polyId]
+        self.w = w
+
+if __name__ == "__main__":
+    import time
+    fname = pysal.examples.get_path('10740.shp')
+    t0 = time.time()
+    c = ContiguityWeights_binning(pysal.open(fname), QUEEN)
+    t1 = time.time()
+    print "using " + str(fname)
+    print "time elapsed for ... using bins: " + str(t1 - t0)
