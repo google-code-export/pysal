00d22d2cb25725747daf7f565b724f983cf7e66f
phil.stphns@gmail.com <phil.stphns@gmail.com@363a8f54-61ba-11de-bb41-d1906ad598ae>
2013-01-14 23:22:48 +0000

    refactoring into a class to begin wih

--- branches/infrastruct/pysal/contrib/db/PGDump.py
+++ branches/infrastruct/pysal/contrib/db/PGDump.py
@@ -8,95 +8,98 @@ from osgeo import ogr
 import pysal
 import os
 
-def db2shape(connstring, input, output):
-    """
-    dumps a postgis database table to shapefile 
+class db_object(connstring):
+    """Class object that holds connection to database and implements
+    methods for interacting with said db."""
 
-    Arguments
-    ---------
-    connstring = A connection string with 4 parameters. Example is 
-    "PG: host='localhost' dbname='pysaldb' user='myusername'
-    password='my_password'"
+    def __init__(connstring):
+        """
+        Instantiates connection to database.
 
-    input : the db table
+        Arguments
+        ---------
+        connstring = A connection string with 4 parameters. Example is 
+        "PG: host='localhost' dbname='pysaldb' user='myusername'
+        password='my_password'"
+        """
+        # which connect utility will you use...ogr? sqlalchemy? django.gis?
+        self.conn = ogr.Open(connstring)
 
-    output : a filename
+    def db2shape(input, output):
+        """
+        dumps a postgis database table to shapefile 
 
-    Examples
-    --------
-    TBD 
-    TODO: make pysal db tables read-only and available on network
+        Arguments
+        ---------
+        input : the db table
 
-    Note
-    ----
+        output : a filename
 
-    If a file exists with the same name as 'output', it will be deleted
-    before being overwritten.
-    """
-    conn = ogr.Open(connstring)
-    layer = conn.GetLayerByName(input)
-    type = layer.GetGeomType()  # returns an int
-    geom_type = OGRGeomType._types[type]  # map int to string description
+        Examples
+        --------
 
-    # Schema definition of SHP file
-    out_driver = ogr.GetDriverByName( 'ESRI Shapefile' )
-    if os.path.exists(output):
-        out_driver.DeleteDataSource(output)
-    out_ds = out_driver.CreateDataSource(output)
-    out_srs = None
-    out_layer = out_ds.CreateLayer(geom_type, out_srs, type)
-    fd = ogr.FieldDefn('name',ogr.OFTString)
-    out_layer.CreateField(fd)
+        Note
+        ----
 
-    #layer = conn.ExecuteSQL(sql)
+        If a file exists with the same name as 'output', it will be deleted
+        before being overwritten.
+        """
+        layer = self.conn.GetLayerByName(input)
+        type = layer.GetGeomType()  # returns an int
+        geom_type = OGRGeomType._types[type]  # map int to string description
 
-    feat = layer.GetNextFeature()
-    while feat is not None:
-        featDef = ogr.Feature(out_layer.GetLayerDefn())
-        featDef.SetGeometry(feat.GetGeometryRef())
-        #featDef.SetField('name',feat.TITLE)
-        out_layer.CreateFeature(featDef)
-        feat.Destroy()
+        # Schema definition of SHP file
+        out_driver = ogr.GetDriverByName( 'ESRI Shapefile' )
+        if os.path.exists(output):
+            out_driver.DeleteDataSource(output)
+        out_ds = out_driver.CreateDataSource(output)
+        out_srs = None
+        out_layer = out_ds.CreateLayer(geom_type, out_srs, type)
+        fd = ogr.FieldDefn('name',ogr.OFTString)
+        out_layer.CreateField(fd)
+
+        #layer = conn.ExecuteSQL(sql)
+
         feat = layer.GetNextFeature()
-    conn.Destroy()
-    out_ds.Destroy()
+        while feat is not None:
+            featDef = ogr.Feature(out_layer.GetLayerDefn())
+            featDef.SetGeometry(feat.GetGeometryRef())
+            #featDef.SetField('name',feat.TITLE)
+            out_layer.CreateFeature(featDef)
+            feat.Destroy()
+            feat = layer.GetNextFeature()
+        conn.Destroy()
+        out_ds.Destroy()
 
-def db_table2gal(connstring, input, weights_type=Contiguity):
-    """
-    generates a GAL file from a postgis database table 
+    def db_table2gal(input, outfile, weights_type=Contiguity):
+        """
+        generates a GAL file from a postgis database table 
 
-    Arguments
-    ---------
-    connstring = A connection string with 4 parameters. Example is 
-    "PG: host='localhost' dbname='pysaldb' user='myusername'
-    password='my_password'"
+        Arguments
+        ---------
+        input : the db table
 
-    input : the db table
+        output: the file to save output
 
-    weights_type : type of spatial weights calculation, defaults to
-    Contiguity
+        weights_type : type of spatial weights calculation, defaults to
+        Contiguity
 
-    Examples
-    --------
-    TBD 
+        Examples
+        --------
+        TBD 
 
-    Note
-    ----
-    TBD
+        pseudo
+        ------
+        connect to db
+        simplify
+        create topo
+        query topogeom  #layer = conn.ExecuteSQL(sql)
+        return aswkt or asgeojson (geom.ExportToWkt())
+        convert returned to pysal w 
+        write w to GAL
 
-    pseudo
-    ------
-    connect to db
-    simplify
-    create topo
-    query topogeom
-    return aswkt or asgeojson
-    convert returned to pysal w 
-    write w to GAL
-
-
-    """
-    pass
+        """
+        pass
 
 
 if __name__ == '__main__':
